2a3ea82100683d65a9dd0dc30df6e0119de6bc52
Zky-Jw <mozabang1@gmail.com>
2026-05-26 00:11:18 +0700

    install vendor

--- config/database.php
+++ config/database.php
@@ -95,15 +95,9 @@ return [
 
        'mongodb' => [
         'driver' => 'mongodb',
-        'host' => env('DB_HOST_MONGO', '127.0.0.1'),
-        'port' => env('DB_PORT_MONGO', 27017),
-        'database' => env('DB_DATABASE_MONGO', 'ssc'),
-        'username' => env('DB_USERNAME_MONGO', ''),
-        'password' => env('DB_PASSWORD_MONGO', ''),
-        'options' => [
-            'database' => env('DB_AUTHENTICATION_DATABASE', 'admin'),
+        'dsn' => env('MONGODB_URI'),
+        'database' => env('MONGODB_DATABASE', 'ssc'),
         ],
-    ],
 
     ],
 
