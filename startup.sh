57d223efae93360649c6f52aa844d93816e74f38
Zky-Jw <mozabang1@gmail.com>
2026-05-28 19:25:46 +0700

    fix: add CIPHERSWEET_KEY to startup.sh

--- startup.sh
+++ startup.sh
@@ -12,7 +12,10 @@ mkdir -p storage/framework/views
 mkdir -p storage/framework/cache
 chmod -R 775 storage bootstrap/cache
 
-# Generate app key jika belum ada (opsional tapi aman)
+# Pastikan CIPHERSWEET_KEY ada di .env jika belum
+grep -q "CIPHERSWEET_KEY" /home/site/wwwroot/.env || echo "CIPHERSWEET_KEY=02d327465b3aac8f18638c96ae14b416d1fc38de315c06fa86edd9c86af8d62f" >> /home/site/wwwroot/.env
+
+# Cache config Laravel
 php artisan config:cache
 php artisan route:cache
 
