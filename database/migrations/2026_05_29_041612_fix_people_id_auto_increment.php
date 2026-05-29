97d0f1c1c3cf695330c8b44dbe42de5ff20c0e95
Zky-Jw <mozabang1@gmail.com>
2026-05-29 11:48:56 +0700

    fix: disable foreign key check saat alter people id

--- database/migrations/2026_05_29_041612_fix_people_id_auto_increment.php
+++ database/migrations/2026_05_29_041612_fix_people_id_auto_increment.php
@@ -9,7 +9,9 @@ return new class extends Migration
 {
     public function up(): void
     {
+        DB::statement('SET FOREIGN_KEY_CHECKS=0');
         DB::statement('ALTER TABLE people MODIFY id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT');
+        DB::statement('SET FOREIGN_KEY_CHECKS=1');
     }
 
     public function down(): void
